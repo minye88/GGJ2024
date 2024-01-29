91ae34c916a71dcea55873a4e09d8b34f92de5ed
minye88 <l.minye@digipen.edu>
2024-01-29 11:50:53 +0800

    Building for WebGL

---
+++ Assets/Scripts/scaleWindow.cs
@@ -0,0 +1,30 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using System.Windows;
+
+// public class scaleWindow : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         window.addEventListener("load", resizePage, false);
+//         window.addEventListener("resize", resizePage, false);
+//     }
+
+//     void resizePage()
+// {  
+//     if (gameInstance != undefined && gameLoaded == true)
+//     {
+//         canvas.style.width = window.innerWidth + "px";
+//         canvas.style.height = window.innerHeight + "px";
+//     }
+   
+// }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
